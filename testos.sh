cba30798426ebd135bd4e872b42962c13cd533be
Guy Bouchard <gboucha@msn.com>
2018-03-07 22:21:40 -0500

    Modify script for OS testing

--- testos.sh
+++ testos.sh
@@ -1,17 +1,35 @@
 #!/bin/bash
 
-OS=$(awk '/DISTRIB_ID=/' /etc/*-release | sed 's/DISTRIB_ID=//' | tr '[:upper:]' '[:lower:]')
-ARCH=$(uname -m | sed 's/x86_//;s/i[3-6]86/32/')
-VERSION=$(awk '/DISTRIB_RELEASE=/' /etc/*-release | sed 's/DISTRIB_RELEASE=//' | sed 's/[.]0/./')
-
-if [ -z "$OS" ]; then
-    OS=$(awk '{print $1}' /etc/*-release | tr '[:upper:]' '[:lower:]')
+if [ -f /etc/os-release ]; then
+    # freedesktop.org and systemd
+    . /etc/os-release
+    OS=$NAME
+    VER=$VERSION_ID
+elif type lsb_release >/dev/null 2>&1; then
+    # linuxbase.org
+    OS=$(lsb_release -si)
+    VER=$(lsb_release -sr)
+elif [ -f /etc/lsb-release ]; then
+    # For some versions of Debian/Ubuntu without lsb_release command
+    . /etc/lsb-release
+    OS=$DISTRIB_ID
+    VER=$DISTRIB_RELEASE
+elif [ -f /etc/debian_version ]; then
+    # Older Debian/Ubuntu/etc.
+    OS=Debian
+    VER=$(cat /etc/debian_version)
+elif [ -f /etc/SuSe-release ]; then
+    # Older SuSE/etc.
+    ...
+elif [ -f /etc/redhat-release ]; then
+    # Older Red Hat, CentOS, etc.
+    ...
+else
+    # Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
+    OS=$(uname -s)
+    VER=$(uname -r)
 fi
 
-if [ -z "$VERSION" ]; then
-    VERSION=$(awk '{print $3}' /etc/*-release)
-fi
+echo OS=$OS
+echo VER=$VER
 
-echo $OS
-echo $ARCH
-echo $VERSION
